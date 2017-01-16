da45516fbe9d13e25bf2125e442293a4d0b70b6f
RANHAOHR <rxh349@case.edu>
2017-01-16 14:26:35 -0500

    add new functios

--- camera_calibration/cmake-build-debug/devel/share/camera_calibration/cmake/camera_calibration-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(camera_calibration_MSG_INCLUDE_DIRS "/home/rxh349/ros_ws/src/Davinci_Calibration/camera_calibration/msg")
-set(camera_calibration_MSG_DEPENDENCIES std_msgs)
