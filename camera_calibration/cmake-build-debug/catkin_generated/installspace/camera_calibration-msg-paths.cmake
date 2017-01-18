991e1eb711fabb11a7ce898e6624f96120d862e2
RANHAOHR <rxh349@case.edu>
2017-01-18 13:42:27 -0500

    fixed a bug for the marker pose computation

--- camera_calibration/cmake-build-debug/catkin_generated/installspace/camera_calibration-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.installspace.in
-
-_prepend_path("${camera_calibration_DIR}/.." "msg" camera_calibration_MSG_INCLUDE_DIRS UNIQUE)
-set(camera_calibration_MSG_DEPENDENCIES std_msgs)
